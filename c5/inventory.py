7575e02963fb61da7a5697f14e080286cf77de79
Thiago <thiago>
2018-03-31 23:25:29 -0300

    C5: addToInventory

--- c5/inventory.py
+++ c5/inventory.py
@@ -1,13 +1,23 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
-        # FILL IN THE CODE HERE
-        #print('k: ' + k)
-        #print('v: ' + str(v))
         print(str(v) + ' ' + k)
+        item_total = item_total + v
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        #print(item)
+        inventory.setdefault(item, 0)
+        inventory[item] = inventory[item] + 1
+
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+
+displayInventory(inv)
